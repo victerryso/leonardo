1c9991170595293c75c9b147ee667ca2b7f4c445
Terry So <victerry.so@gmail.com>
2024-11-17 23:12:06 +1100

    updated avatar

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,7 +2,7 @@ import { Container, Flex, Heading, Separator } from "@chakra-ui/react";
 import Link from "next/link";
 import { ROUTES } from "../constants";
 import { Avatar } from "./chakra/avatar";
-import AuthContext from "../contexts/auth-context";
+import AuthContext, { isAuthenticated } from "../contexts/auth-context";
 import { useContext } from "react";
 
 const Header = () => {
@@ -16,7 +16,11 @@ const Header = () => {
             <Link href={ROUTES.HOME}>Rick and Morty</Link>
           </Heading>
           <Link href={ROUTES.REGISTER}>
-            <Avatar name={auth?.username} />
+            {isAuthenticated(auth) ? (
+              <Avatar name={auth.username} />
+            ) : (
+              <Avatar />
+            )}
           </Link>
         </Flex>
       </Container>
